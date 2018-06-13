6328fe42f47f9de9329f1ead09febe46a55ae4a4
Unknown <soph.bohr22@gmail.com>
2018-06-13 13:10:00 -0500

    created activity

---
+++ FileIOActivity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileIOActivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
